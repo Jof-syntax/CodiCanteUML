1922428ea1f7b520dd37f6e6e37d389e2f6fee33
Jof-syntax <70135822+Jof-syntax@users.noreply.github.com>
2021-11-10 19:53:57 +0100

    new test

--- Testing/Tests/TestGliderAnalyser.m
+++ Testing/Tests/TestGliderAnalyser.m
@@ -2,7 +2,7 @@ function TestGliderAnalyser
 InputData = load('TestData/Input data/TestClassDataGliderAnalyser.mat').cParams;
 test = GliderAnalyser(InputData);
 test.compute();
-expectedResult = load('TestData/Output data/TestClassDataGliderAnalyserResult.mat').test;
+expectedResult = load('TestData/Outputdata/ResultTestGliderAnalyser.mat').test;
 computeError(test, expectedResult, 'TestGliderAnalyser');
 end
 
