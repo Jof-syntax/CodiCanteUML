4b00079ea13b1c683450c7cec6c792930ca640ea
Jof-syntax <70135822+Jof-syntax@users.noreply.github.com>
2021-11-10 17:24:08 +0100

    updated class

--- Code/DynamicsSolver/Forces/ForcesComputer.m
+++ Code/DynamicsSolver/Forces/ForcesComputer.m
@@ -6,6 +6,11 @@ classdef ForcesComputer < handle
     
     properties (Access = private)
         data
+        cg
+        conservativeForces
+        Lift
+        Drag
+        Thrust
     end
     
     
@@ -16,16 +21,85 @@ classdef ForcesComputer < handle
         end
         
         function compute(obj)
+            obj.computeForces();
+            obj.computeNodeForce();
         end
         
     end
     
     methods (Access = private)
         
+        
         function init(obj, cParams)
-            obj.cParams = cParams;
+            obj.data = cParams;
         end
         
+        function computeForces(obj)
+            obj.computeConservativeForces();
+            obj.computeCG();
+            obj.computeNotConservativeForces();
+        end
+             
+        function computeConservativeForces(obj)
+            s = obj.createConservativeForcesComputer();
+            a = ConservativeForcesComputer(s);
+            a.compute();
+            obj.conservativeForces = a.conservativeForces;
+        end
+        
+        function computeCG(obj)
+            s = createCGComputer(obj);
+            a = CGComputer(s);
+            a.compute();
+            obj.cg = a.cg;
+        end
+        
+        function computeNotConservativeForces(obj)
+            s = createNotConservativeF(obj);
+            a = NotConservativeForcesComputer(s);
+            a.compute();
+            obj.Lift    = a.Lift;
+            obj.Drag    = a.Drag;
+            obj.Thrust  = a.Thrust;
+        end
+        
+        function s = createNotConservativeF(obj)
+            s.data                    = obj.data;
+            s.data.cg                 = obj.cg;
+            s.data.conservativeForces = obj.conservativeForces;
+        end
+        
+        function s = createCGComputer(obj)
+            s.conservativeForces = obj.conservativeForces;
+            s.pilotWeight        = obj.data.pilotWeight;
+            s.dim                = obj.data.dim;
+            s.g                  = obj.data.g;
+            s.x                  = obj.data.x;
+            s.mass               = obj.data.mass;
+        end
+        
+        function s = createConservativeForcesComputer(obj)
+            s = obj.data;
+        end
+        
+        function computeNodeForceComputer(obj)
+            s = obj.createNodeForceComputer();
+            a = NodeForceComputer(s);
+            a.compute();
+            obj.forces = a.forces;
+        end
+                
+        function s = createNodeForceComputer(obj)
+            s.gliderForces  = obj.gliderForces;
+            s.aceleration   = obj.aceleration;
+            s.pilotWeight   = obj.data.pilotWeight;
+            s.mat           = obj.data.mat;
+            s.tMat          = obj.data.tMat;
+            s.dim           = obj.data.dim;
+            s.tN            = obj.data.tN;
+            s.fData         = obj.conservativeForces;
+            s.x             = obj.data.x;
+        end
         
     end
     
